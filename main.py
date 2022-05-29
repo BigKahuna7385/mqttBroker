9269a42db465e2eb80b17cebeefea984809a1385
lkno0705 <lkno0705@gmail.com>
2022-05-29 16:25:05 +0200

    Add Diagrams

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class A:
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    def __init__(self ,arnold, anna):
+        self.arnold = arnold
+        self._anna = anna
+        self.list = ["A", "B"]
+        self.dict = {"A": 1}
 
+    def get_arnold(self):
+        return self.arnold
+
+    def _get_anna(self):
+        return self._anna
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    a = A("arnold", "anna")
+    print(a.get_arnold())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
